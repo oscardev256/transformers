05e0f167f487c46b978246084a240b37226ca2de
Oscar Gonzalez <ogonzal6@alumni.jh.edu>
2025-07-04 09:43:04 -0400

    Added second layer to audio projection module

--- src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
+++ src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
@@ -2310,7 +2310,7 @@ class Qwen2VLAudioForConditionalGeneration(Qwen2VLForConditionalGeneration):
         #)
 
         # Two-layer projection: d_model → intermediate_dim → hidden_size
-        intermediate_dim = config.hidden_size/2 # You can pick a reasonable size like 512 or 1024
+        intermediate_dim = config.hidden_size//2 # You can pick a reasonable size like 512 or 1024
 
         self.audio_proj = nn.Sequential(
             nn.Linear(whisper_model.config.d_model, intermediate_dim, bias=False),
