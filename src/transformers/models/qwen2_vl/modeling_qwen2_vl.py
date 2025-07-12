acb5cdb1d8ae343fa6dd667f45151aa075959708
Oscar Gonzalez <ogonzal6@alumni.jh.edu>
2025-07-11 21:41:19 -0400

    Integrated audio encoder into model skeleton.

--- src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
+++ src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
@@ -2077,6 +2077,8 @@ class Qwen2VLAudioForConditionalGeneration(Qwen2VLForConditionalGeneration):
         #whisper_cfg = WhisperConfig.from_dict(config.audio_config.to_dict())
         whisper_cfg = WhisperConfig()
         self.audio_encoder = WhisperEncoder(whisper_cfg)
+        self.audio_proj   = nn.Linear(whisper_cfg.config.d_model, config.hidden_size, bias=False)
+
         '''
         # ------------------- Whisper encoder -------------------
         whisper_model = WhisperModel.from_pretrained("openai/whisper-tiny")
